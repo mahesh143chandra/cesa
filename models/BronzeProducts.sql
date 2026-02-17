eb337b62af16484c69068472d4cc3eec3207496a
mahesh143chandra <chandramahesh098@gmail.com>
2026-02-17 22:45:15 +1100

    'first'

---
+++ models/BronzeProducts.sql
@@ -0,0 +1,5 @@
+{{ config(materialized='table') }}
+
+
+select * from workspace.default.products
+
---
+++ models/Bronzeemployees.sql
@@ -0,0 +1,6 @@
+{{ config(materialized='table') }}
+
+
+select * from workspace.default.employees
+
+
